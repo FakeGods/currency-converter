33be2d17c301875354becd8be5dbe15b10da1ef5
wiktor_leszkowicz <w.leszkowicz99@gmail.com>
2023-09-18 16:56:52 +0200

    fix v1

--- index.js
+++
@@ -1,7 +0,0 @@
-const multiplyBy = (arr, number) => {
-  return arr.map((element) => element * number);
-  
-};
-const numbers = [1, 2, 3];
-const multipliedNumbers = multiplyBy(numbers, 2);
-console.log(multipliedNumbers);
